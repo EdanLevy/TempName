0af8c49d396cbf5b9ecd8f4a428333f64523c759
Elad Inbar <eladin@post.bgu.ac.il>
2021-12-22 17:22:08 +0200

    Added broadcast message constant variables. Added encode, decode format constant variable. Randomized server port number (port number is sent to clients during broadcast). Implemented the function send_broadcast. Fixed 'while' condition. Encoded the 'send_to_client' message. Added decode in 'accept_client'.

--- src/Server.py
+++ src/Server.py
@@ -5,18 +5,22 @@ import time
 from Session import Session
 from Client import Client
 
-REBROADCAST_TIMEOUT = 1
+REBROADCAST_TIMEOUT = 1  # Broadcast announcement timeout after which another broadcast is sent (1 second)
 
 SERVER_IP = socket.gethostbyname(socket.gethostname())  # Acquire local host IP address
 
+MAGIC_COOKIE = "0xabcddcba"  # All broadcast offer messages MUST begin with this prefix
+MESSAGE_TYPE = "0x2"  # Specifies broadcast offer, no other message types are supported
 BROADCAST_DST_PORT = 13117  # Fixed port number, as defined in the packet formats
 BROADCAST_SRC_PORT = random.randint(1024, 65535)  # The port from which to send out offer announcements
 
-SERVER_PORT = 33333  # The port from which the server will listen for incoming client connections
+# The port from which the server will listen for incoming client connections
+SERVER_PORT = random.choice([i for i in range(1024, 65535) if i not in [BROADCAST_SRC_PORT]])
 
 SERVER_ADDR = (SERVER_IP, SERVER_PORT)
 BROADCAST_SERVER_ADDR = (SERVER_IP, BROADCAST_SRC_PORT)
 
+FORMAT = 'utf-8'  # Decode and encode format for incoming and outgoing messages
 MAX_CLIENTS = 2  # Amount of clients required to initiate a game session
 
 clients: list = []
@@ -24,13 +28,14 @@ accept_thread = None
 
 
 def send_broadcast(udp_socket):
-    # TODO - define and send offer announcements according to 'Packet Formats'
-    pass
+    announcement_message = MAGIC_COOKIE + MESSAGE_TYPE + str(SERVER_PORT)
+    udp_socket.send(announcement_message.encode(FORMAT))
 
 
 # accept client to the session if available, and wait to start the session
 def accept_client(connection_socket, address):
-    client_name = connection_socket.recv(1024)
+    client_name = connection_socket.recv(1024)  # First message from client is expected to be their name
+    client_name = client_name.decode(FORMAT).split('\n')[0]
     if len(clients) < MAX_CLIENTS:
         clients.append(Client(socket=connection_socket, address=address, name=client_name))
 
@@ -48,14 +53,14 @@ def open_tcp_server():
 
 
 def listen_for_clients(server_socket):
-    while not len(clients) < MAX_CLIENTS:
+    while len(clients) < MAX_CLIENTS:
         conn, address = server_socket.accept()
         accept_client(conn, address)
 
 
 def send_to_client(client: Client, message: str) -> None:
     conn = client.socket
-    conn.send(message)
+    conn.send(message.encode(FORMAT))
 
 
 def start() -> None:
@@ -65,12 +70,14 @@ def start() -> None:
         # and socket kind is SOCK_DGRAM (connectionless UDP)
         broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         broadcast_socket.bind(BROADCAST_SERVER_ADDR)
+        # Start a new thread to listen to client connections
         threading.Thread(target=listen_for_clients, args=[server_socket]).start()
-        while not len(clients) != MAX_CLIENTS:
+        # Meanwhile, send offer announcements
+        while len(clients) < MAX_CLIENTS:
             send_broadcast(broadcast_socket)
             time.sleep(REBROADCAST_TIMEOUT)
         broadcast_socket.close()
+        # All clients have connected, initialize game session
         session = Session(send_handler=send_to_client)
         session.add_players(clients)
-
         session.begin_game()
