0dc231df3fff91213b2c4ee353ec5debfd173292
Elad Inbar <eladin@post.bgu.ac.il>
2021-12-28 21:27:36 +0200

    Welcome message and math problem were supposed to be sent as a single TCP message, merged both functions to allow this. Fixed results assignment to use the appropriate get function for maps.

--- src/server/Session.py
+++ src/server/Session.py
@@ -26,7 +26,7 @@ class Session:
         for sock in read_ready:
             player = self.p1 if sock is self.p1.socket else self.p2
             answer = self.receive_handler(sock)
-            self.results[player][0] = answer
+            self.results.get(player)[0] = answer
             break  # Once 1 player has sent an answer, the game is decided
         self.check_send_result(player)
 
@@ -50,14 +50,11 @@ class Session:
                   f"Congratulations to the winner: {self.the_winner}\n"
         self.send_message_to_players(message)
 
-    def send_welcome_message(self):
-        message = f'Welcome to Quick Maths.\nPlayer 1: {self.p1.name}.\nPlayer 2: {self.p2.name}\n==\n'
-        self.send_message_to_players(message)
-
-    def send_math_problem(self):
-        pre_message = "Please answer the following question as fast as you can:\n"
+    def send_game_messages(self):
+        welcome_message = f'Welcome to Quick Maths.\nPlayer 1: {self.p1.name}.\nPlayer 2: {self.p2.name}\n==\n' \
+                          f'Please answer the following question as fast as you can:\n'
         question = "How much is " + self.the_question + "?\n"
-        message = pre_message + question
+        message = welcome_message + question
         self.send_message_to_players(message)
 
     def finish_game(self):
@@ -66,8 +63,7 @@ class Session:
 
     def begin_game(self):
         time.sleep(self.GAME_BEGINS_DELAY)
-        self.send_welcome_message()
-        self.send_math_problem()
+        self.send_game_messages()
         self.receive_answers()
         self.send_result()
         self.finish_game()
