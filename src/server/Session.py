3672ace70481d2dc0e5a672da18bdb5fe2acef8a
Elad Inbar <eladin@post.bgu.ac.il>
2021-12-28 13:50:43 +0200

    Changed from threading to select

--- src/server/Session.py
+++ src/server/Session.py
@@ -30,8 +30,25 @@ class Session:
             break  # Once 1 player has sent an answer, the game is decided
         self.check_send_result(player)
 
+    def check_send_result(self, p: Player):
+        if p is None:
+            self.the_winner = None
+        else:
+            actual = int(self.results[p][0])
+
+            if actual == self.the_answer:
+                self.the_winner = p.name
+            else:
+                self.the_winner = self.p2.name if p == self.p1 else self.p1.name
+
+    def send_message_to_players(self, message: str):
+        self.send_handler(self.p1, message)
+        self.send_handler(self.p2, message)
+
     def send_result(self):
-        pass
+        message = f"Game over!\nThe correct answer was  {self.the_answer}!\n\n  " \
+                  f"Congratulations to the winner: {self.the_winner}\n"
+        self.send_message_to_players(message)
 
     def send_welcome_message(self):
         message = f'Welcome to Quick Maths.\nPlayer 1: {self.p1.name}.\nPlayer 2: {self.p2.name}\n==\n'
