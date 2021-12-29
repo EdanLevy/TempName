432d86f28a498b75a1d602cf55d8a45d13cbc0fd
Elad Inbar <eladin@post.bgu.ac.il>
2021-12-29 21:47:38 +0200

    oooff

--- src/server/Server.py
+++ src/server/Server.py
@@ -11,7 +11,7 @@ from Player import Player
 REBROADCAST_TIMEOUT = 1  # Broadcast announcement timeout after which another broadcast is sent (1 second)
 
 SERVER_IP = "127.0.0.1"  # Acquire local host IP address
-SERVER_IP_DEV_NETWORK = "172.18.0.90"  # Dev network
+SERVER_IP_DEV_NETWORK = "172.1.0.90"  # Dev network
 SERVER_IP_TEST_NETWORK = "172.99.0.90"  # Test network - only to be used when being graded
 
 BROADCAST_DST_PORT = 13117  # Fixed port number, as defined in the packet formats
